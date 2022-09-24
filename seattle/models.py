522817d35b736f406ec22444713ed30bbc121c70
DesertJedi <brandnewruss@gmail.com>
2022-09-24 16:22:34 -0700

    models

--- seattle/models.py
+++ seattle/models.py
@@ -1,4 +1,18 @@
 from django.db import models
 
 class SpdSelectAll(models.Model):
-    
+    ID = models.CharField(max_length=255, primary_key=True)
+    Incident_Num = models.IntegerField()
+    Incident_Type = models.CharField(max_length=255)
+    Occured_date_time = models.DateField()
+    Precinct = models.CharField(max_length=255)
+    Sector = models.CharField(max_length=255)
+    Beat = models.CharField(max_length=255)
+    Officer_ID = models.IntegerField()
+    Subject_ID = models.IntegerField()
+    Subject_Race = models.CharField(max_length=255)
+    Subject_Gender = models.CharField(max_length=255)
+
+    class Meta:
+        managed = False
+        db_table = 'spd'
